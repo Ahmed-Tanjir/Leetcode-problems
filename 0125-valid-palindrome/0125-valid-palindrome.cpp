2395975679a0558c155a841fa88d246e30471ff5
Ahmed-Tanjir <tanjir358526@gmail.com>
2026-06-29 00:45:00 +0600

    Time: 1 ms (43.92%), Space: 9.8 MB (77.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         if(s.size()-1 == 0) return true;
+
         int i = 0;
         for (char c : s) {
             if (isalnum(c)) {
-                s[i] = c;
+                tolower(c);
+                s[i] = tolower(c);
                 i++;
             }
         }
 
         s.resize(i);
-
-         transform(s.begin(), s.end(), s.begin(), ::tolower);
-
+        
         int start = 0, end = s.size()-1;
-        bool isvalid = true;
-
         while(start<=end){
             if(s[start] != s[end]){
                 return false;
@@ -24,8 +22,6 @@ public:
             start++;
             end--;
         }
-
-        return isvalid;
-        
+        return true;  
     }
 };
